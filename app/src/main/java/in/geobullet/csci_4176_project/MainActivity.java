3ac457f3239dc842401f61b972401596aae58550
Nick Barron <nickbarron16@gmail.com>
2017-02-23 23:11:22 -0400

    Misc labels

--- app/src/main/java/in/geobullet/csci_4176_project/MainActivity.java
+++ app/src/main/java/in/geobullet/csci_4176_project/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Do somethin, yo!", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Well, hello there!", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
