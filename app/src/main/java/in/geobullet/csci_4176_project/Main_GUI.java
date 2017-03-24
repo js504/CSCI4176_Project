c3baeb623cf0560756657a02dce5ebd5beb09c18
Nick Barron <nickbarron16@gmail.com>
2017-03-24 20:05:22 -0300

    Commented out database code in Main_GUI, not completed yet

--- app/src/main/java/in/geobullet/csci_4176_project/Main_GUI.java
+++ app/src/main/java/in/geobullet/csci_4176_project/Main_GUI.java
@@ -3,9 +3,8 @@ package in.geobullet.csci_4176_project;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -13,13 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.HorizontalScrollView;
-
-import java.util.List;
-
-import in.geobullet.csci_4176_project.db.Classes.Board;
-import in.geobullet.csci_4176_project.db.Classes.Poster;
-import in.geobullet.csci_4176_project.db.DatabaseHandler;
+import android.view.View;
 
 public class Main_GUI extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -30,32 +23,32 @@ public class Main_GUI extends AppCompatActivity
         setContentView(R.layout.activity_main__gui);
 
 
-        int BOARD_ID = 1;
-
-        final DatabaseHandler dbHandler = new DatabaseHandler(this);
-
-        Board board = dbHandler.getBoardById(BOARD_ID);
-
-        if (board != null) {
-
-            // todo: Use board to set page title, radius,
-
-            List<Poster> postersForBoard1 = dbHandler.getPostersForBoard(BOARD_ID);
-
-            HorizontalScrollView hScrollView = (HorizontalScrollView) findViewById(R.id.horizontal_scroll_view);
-
-            for (Poster p : postersForBoard1) {
-
-                // todo finish
-
-                //ImageView iv = new ImageView();
-
-                // something like this
-                //iv.setImage(p.getPhotoName());
-
-                //hScrollView.addView(iv);
-            }
-        }
+//        int BOARD_ID = 1;
+//
+//        final DatabaseHandler dbHandler = new DatabaseHandler(this);
+//
+//        Board board = dbHandler.getBoardById(BOARD_ID);
+//
+//        if (board != null) {
+//
+//            // todo: Use board to set page title, radius,
+//
+//            List<Poster> postersForBoard1 = dbHandler.getPostersForBoard(BOARD_ID);
+//
+//            HorizontalScrollView hScrollView = (HorizontalScrollView) findViewById(R.id.horizontal_scroll_view);
+//
+//            for (Poster p : postersForBoard1) {
+//
+//                // todo finish
+//
+//                //ImageView iv = new ImageView();
+//
+//                // something like this
+//                //iv.setImage(p.getPhotoName());
+//
+//                //hScrollView.addView(iv);
+//            }
+//        }
 
 
 
