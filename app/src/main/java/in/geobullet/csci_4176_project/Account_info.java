627029bed888f4c5535a45595c516f532c10c352
tianyewang <tn610582@dal.ca>
2017-03-29 19:37:31 -0300

    Update the adapter and Account information. The account information now display the posters created by the user that logged in. Edit user information now complete, user can update their information. Note: the memory issue is caused by the resolution of the posters. Can be fixed by resize the poster but view poster are not looking prrety.

--- app/src/main/java/in/geobullet/csci_4176_project/Account_info.java
+++ app/src/main/java/in/geobullet/csci_4176_project/Account_info.java
@@ -6,6 +6,7 @@
 
 package in.geobullet.csci_4176_project;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -21,10 +22,15 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
 
 import in.geobullet.csci_4176_project.Utils.NavViewListener;
+import in.geobullet.csci_4176_project.db.Classes.Poster;
 import in.geobullet.csci_4176_project.db.Classes.User;
 import in.geobullet.csci_4176_project.db.DatabaseHandler;
 
@@ -49,24 +55,56 @@ public class Account_info extends AppCompatActivity {
             Log.i("id",Integer.toString(id));
         }
 
-        TextView username = (TextView) findViewById(R.id.editUsername);
-        TextView first_name = (TextView) findViewById(R.id.edit_firstname);
-        TextView last_name = (TextView) findViewById(R.id.editLastname);
-        TextView email = (TextView) findViewById(R.id.edit_Email);
+
+        final TextView username = (TextView) findViewById(R.id.editUsername);
+        final TextView first_name = (TextView) findViewById(R.id.edit_firstname);
+        final TextView last_name = (TextView) findViewById(R.id.editLastname);
+        final TextView email = (TextView) findViewById(R.id.edit_Email);
+        final TextView oldpwd = (TextView) findViewById(R.id.editOldpwd);
+        final TextView newpwd = (TextView) findViewById(R.id.edit_Newpwd);
 
 
-        User currentUser = SessionData.currentUser;
-
+        final User currentUser = SessionData.currentUser;
+        List<Poster> userPoster = null;
         if (currentUser != null) {
             username.setText(currentUser.getDisplayName());
             first_name.setText(currentUser.getFirstName());
             last_name.setText(currentUser.getLastName());
             email.setText(currentUser.getEmail());
+            userPoster = db.getPostersForUser(currentUser.getId());
         }
 
 
+        //button to update the user's information
+        Button submit_button = (Button) findViewById(R.id.submit_changes);
+        submit_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                assert currentUser != null;
+                if(oldpwd.getText().toString().equals(currentUser.getPassword())) {
+                    currentUser.setDisplayName(username.getText().toString());
+                    currentUser.setFirstName(first_name.getText().toString());
+                    currentUser.setLastName(last_name.getText().toString());
+                    currentUser.setEmail(email.getText().toString());
+                    currentUser.setPassword(newpwd.getText().toString());
+
+                    db.updateUser(currentUser);
+                    Intent intent = new Intent(Account_info.this, Account_info.class);
+                    finish();
+                    startActivity(intent);
+                }
+                else{
+                    //showing toast message alert user that the original password not match when trying to change user information
+                    Context context = getApplicationContext();
+                    CharSequence text = "Original password not valid!";
+                    int duration = Toast.LENGTH_SHORT;
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                }
+            }
+        });
+
         ListView lv=(ListView) findViewById(R.id.listView);
-        lv.setAdapter(new CustomAdapter(this, prgmNameList,prgmImages));
+        lv.setAdapter(new CustomAdapter(this, userPoster));
 
         //add click listener to the list view of posters when click call CreateNewPoster activity and pass in poster object
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -80,6 +118,10 @@ public class Account_info extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+
+
 
         //menu component
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
--- app/src/main/java/in/geobullet/csci_4176_project/CustomAdapter.java
+++ app/src/main/java/in/geobullet/csci_4176_project/CustomAdapter.java
@@ -5,6 +5,7 @@ package in.geobullet.csci_4176_project;
  */
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,13 +13,17 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.List;
+
+import in.geobullet.csci_4176_project.db.Classes.Poster;
+
 
 public class CustomAdapter extends BaseAdapter{
     private String [] posternames;
     private Context context;
-    private int [] posters;
+    private List<Poster> posters;
     private static LayoutInflater inflater=null;
-    public CustomAdapter(Account_info mainActivity, String[] posternames, int[] posters) {
+    public CustomAdapter(Account_info mainActivity, List<Poster> posters) {
         this.posternames =posternames;
         context=mainActivity;
         this.posters =posters;
@@ -26,7 +31,7 @@ public class CustomAdapter extends BaseAdapter{
     }
     @Override
     public int getCount() {
-        return posternames.length;
+        return posters.size();
     }
 
     @Override
@@ -51,8 +56,14 @@ public class CustomAdapter extends BaseAdapter{
         listview = inflater.inflate(R.layout.custom_poster_listview, null);
         holder.tv=(TextView) listview.findViewById(R.id.textView1);
         holder.img=(ImageView) listview.findViewById(R.id.imageView1);
-        holder.tv.setText(posternames[position]);
-        holder.img.setImageResource(posters[position]);
+        holder.tv.setText(posters.get(position).getTitle());
+
+        String name =posters.get(position).getPhotoName();
+        name = name.substring(0, name.lastIndexOf("."));
+        int id = context.getResources().getIdentifier(name, "drawable", context.getPackageName());
+        Log.i("name", Integer.toString(id));
+        //c.getResources().getIdentifier(ImageName, "drawable", c.getPackageName());
+        holder.img.setImageResource(id);
         return listview;
     }
 
