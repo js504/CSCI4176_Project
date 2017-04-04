c3eb7c4e4bebea26088d10f39ea677e2044df27a
Darrell <darrell@mail.com>
2017-04-03 21:58:25 -0300

    Applied poster placement logic
    todo:   add cost field to db
    apply more polish!!!

--- app/src/main/java/in/geobullet/csci_4176_project/Main_GUI.java
+++ app/src/main/java/in/geobullet/csci_4176_project/Main_GUI.java
@@ -22,8 +22,9 @@ import in.geobullet.csci_4176_project.Database.Classes.Board;
 import in.geobullet.csci_4176_project.Database.Classes.Poster;
 import in.geobullet.csci_4176_project.Database.Classes.PosterType;
 import in.geobullet.csci_4176_project.Database.DatabaseHandler;
-import in.geobullet.csci_4176_project.Database.Utils.DateUtil;
+
 import in.geobullet.csci_4176_project.Shared.SessionData;
+import in.geobullet.csci_4176_project.Utils.DateUtil;
 import in.geobullet.csci_4176_project.Utils.NavMenuManager;
 import in.geobullet.csci_4176_project.Utils.NavViewListener;
 
@@ -56,6 +57,9 @@ public class Main_GUI extends AppCompatActivity implements View.OnClickListener{
         List<Poster> postersForBoard1 = dbHandler.getPostersForBoard(board.getId());
         int left_margin_index = 0;
         int top_margin_index = 0;
+        int column_index = 0;
+        int row_index = 0;
+        int poster_count = 0;
         String poster_name = null;
 
         if (board != null) {
@@ -77,21 +81,37 @@ public class Main_GUI extends AppCompatActivity implements View.OnClickListener{
                     ib.setImageResource(resID);
                     ib.setId(p.getId());
 
-                    if(left_margin_index == 0)
+                    if(poster_count == 0)
                     {
-                        params.setMargins(0, 50, 0, 0);
+                        poster_count++;
+                        params.setMargins(left_margin_index, top_margin_index, 0, 0);
                         ib.setLayoutParams(params);
                         RelLayout.addView(ib);
+                        row_index++;
                     }
                     else
                     {
-                        params.setMargins(left_margin_index*520, 50, 0, 0);
-                        ib.setLayoutParams(params);
-                        RelLayout.addView(ib);
-
+                        if(row_index <=2)
+                        {
+                            top_margin_index = (230*row_index);
+                            left_margin_index = (520*column_index);
+                            params.setMargins(left_margin_index, top_margin_index, 0, 0);
+                            ib.setLayoutParams(params);
+                            RelLayout.addView(ib);
+                            row_index++;
+                        }
+                        else
+                        {
+                            column_index++;
+                            row_index = 0;
+                            left_margin_index = (520*column_index);
+                            top_margin_index = (230*row_index);
+                            params.setMargins(left_margin_index, top_margin_index, 0, 0);
+                            ib.setLayoutParams(params);
+                            RelLayout.addView(ib);
+                            row_index++;
+                        }
                     }
-                    top_margin_index++;
-                    left_margin_index++;
                 }
                 else if(selected_poster_type.equals("Event"))
                 {
@@ -110,20 +130,37 @@ public class Main_GUI extends AppCompatActivity implements View.OnClickListener{
                         ib.setImageResource(resID);
                         ib.setId(p.getId());
 
-                        if(left_margin_index == 0)
+                        if(poster_count == 0)
                         {
-                            params.setMargins(0, 50, 0, 0);
+                            poster_count++;
+                            params.setMargins(left_margin_index, top_margin_index, 0, 0);
                             ib.setLayoutParams(params);
                             RelLayout.addView(ib);
+                            row_index++;
                         }
                         else
                         {
-                            params.setMargins(left_margin_index*520, 50, 0, 0);
-                            ib.setLayoutParams(params);
-                            RelLayout.addView(ib);
+                            if(row_index <=2)
+                            {
+                                top_margin_index = (230*row_index);
+                                left_margin_index = (520*column_index);
+                                params.setMargins(left_margin_index, top_margin_index, 0, 0);
+                                ib.setLayoutParams(params);
+                                RelLayout.addView(ib);
+                                row_index++;
+                            }
+                            else
+                            {
+                                column_index++;
+                                row_index = 0;
+                                left_margin_index = (520*column_index);
+                                top_margin_index = (230*row_index);
+                                params.setMargins(left_margin_index, top_margin_index, 0, 0);
+                                ib.setLayoutParams(params);
+                                RelLayout.addView(ib);
+                                row_index++;
+                            }
                         }
-                        top_margin_index++;
-                        left_margin_index++;
                     }
                 }
                 else        //selected_poster_type == "Service"
@@ -143,20 +180,37 @@ public class Main_GUI extends AppCompatActivity implements View.OnClickListener{
                         ib.setImageResource(resID);
                         ib.setId(p.getId());
 
-                        if(left_margin_index == 0)
+                        if(poster_count == 0)
                         {
-                            params.setMargins(0, 50, 0, 0);
+                            poster_count++;
+                            params.setMargins(left_margin_index, top_margin_index, 0, 0);
                             ib.setLayoutParams(params);
                             RelLayout.addView(ib);
+                            row_index++;
                         }
                         else
                         {
-                            params.setMargins(left_margin_index*520, 50, 0, 0);
-                            ib.setLayoutParams(params);
-                            RelLayout.addView(ib);
+                            if(row_index <=2)
+                            {
+                                top_margin_index = (230*row_index);
+                                left_margin_index = (520*column_index);
+                                params.setMargins(left_margin_index, top_margin_index, 0, 0);
+                                ib.setLayoutParams(params);
+                                RelLayout.addView(ib);
+                                row_index++;
+                            }
+                            else
+                            {
+                                column_index++;
+                                row_index = 0;
+                                left_margin_index = (520*column_index);
+                                top_margin_index = (230*row_index);
+                                params.setMargins(left_margin_index, top_margin_index, 0, 0);
+                                ib.setLayoutParams(params);
+                                RelLayout.addView(ib);
+                                row_index++;
+                            }
                         }
-                        top_margin_index++;
-                        left_margin_index++;
                     }
                 }
             }
